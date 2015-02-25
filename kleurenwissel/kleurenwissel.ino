7b2d5aaa0c9df0891e86870adf5c9c75b2eaa5d2
Damir Cuturic <damian.damir+github@gmail.com>
2015-02-25 11:34:57 +0100

    output values to display

--- kleurenwissel/kleurenwissel.ino
+++ kleurenwissel/kleurenwissel.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
 
-const int potentiometer = A0;
+const int potentiometer = A0;      
 
 rgb_lcd lcd;
 
@@ -10,7 +10,6 @@ int green;
 int blue;
 
 int pin1 = 0;
-//int pin2=1;
 
 void setup()
 {
@@ -18,21 +17,26 @@ void setup()
   pinMode(potentiometer, INPUT);
   lcd.begin(16, 2);
   lcd.print("I change colors!");
-
 }
 
 void loop()
 {
   red = analogRead(pin1);
-  red /= 12;
-  green = red - 85;
-
-  green = analogRead(pin1);
-  blue = green - 170;
+  red /= 12;                      // convert value from 0-1023 to 0-255 (red/4); and divides the range in 3 equal parts (3 * 4 = 12)
+  green = red - 85;               // green = red - (1024/12)
+//green = analogRead(pin1);       // overblijfsel uit eerdere probeersels, negeer deze regel
+  blue = green;
+//blue = green - 85;              // overblijfsel uit eerdere probeersels, negeer deze regel
 
   lcd.setRGB(red, green, blue);
-  Serial.println(blue);              // Displays the values of the selected color in the "Serial Monitor" :: red, green, blue are possile inputs
-  delay(1000);
+//Serial.println(blue);           // bekijk de waarden van de kleur terwijl je draait, red|green|blue, onderstaande weergeeft het direct op het LCDscherm
+  lcd.setCursor(0, 1);
+  lcd.print(red);
+  lcd.print("|");
+  lcd.print(green );
+  lcd.print("|");
+  lcd.print(blue);
+  delay(100);
 }
 
 
