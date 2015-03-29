030e0dcc58f9d7e1a07673eee32f287d30e712e1
Damir Cuturic <damian.damir+github@gmail.com>
2015-03-29 23:19:01 +0200

    added button control

--- Ping_Pong_Bang/Ping_Pong_Bang.ino
+++ Ping_Pong_Bang/Ping_Pong_Bang.ino
@@ -8,21 +8,41 @@
 
 */
 AF_DCMotor motor1(1, MOTOR12_64KHZ);     // Create motor Object at port 1, with frequency
-AF_DCMotor motor2(2, MOTOR12_64KHZ);     //
+AF_DCMotor motor2(2, MOTOR12_64KHZ);     // Create motor Object at port 2, with frequency
+const int buttonPin = 2; // set
+int buttonstate = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-Serial.print("Hello MoTo");
-motor1.setSpeed(255);
-motor2.setSpeed(255);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+  Serial.print("Hello MoTo");
+  Serial.print("\n");
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- Serial.print("Going Forward!")
- Serial.print("\n")
- motor1.run(FORWARD); // turn it on going forward
- motor2.run(FORWARD); // turn it on going forward
- delay(1000);
+  Serial.println("Going Forward!");
+  buttonstate = digitalRead(buttonPin);
+  if (buttonstate == HIGH) {
+    runrun();
+    Serial.println("Wait 3 sec");
+    delay(3000);
+    buttonstate = 0;
+  }
+}
+
+void runrun() {
+  Serial.print("\n");
+  Serial.print("Go!");
+    motor1.run(FORWARD); // turn it on going forward
+    motor2.run(BACKWARD); // turn it on going forward
+  delay(2000);
+  Serial.print("Stop!");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
 }
